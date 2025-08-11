e87ae5fbe35993b96657b0bd61e88544ab881f97
PolyCat <contact.polycat@proton.me>
2025-08-11 17:53:20 +0300

    made script also generate a .clangd file

--- Setup-PlatfromIO-Clangd/setup-pio-clangd.py
+++ Setup-PlatfromIO-Clangd/setup-pio-clangd.py
@@ -26,5 +26,37 @@ with open("extra_script.py", "w") as f:
 with open("platformio.ini", "a") as f:
     _ = f.write("extra_scripts = pre:extra_script.py")
 
+clangd_config = """
+CompileFlags:
+  Add: [
+    -DSSIZE_MAX,
+    -DLWIP_NO_UNISTD_H=1,
+    -Dssize_t=long,
+    -D_SSIZE_T_DECLARED,
+    -Wno-unknown-warning-option
+  ]
+  Remove: [
+    -mlong-calls,
+    -fno-tree-switch-conversion,
+    -mtext-section-literals,
+    -mlongcalls,
+    -fstrict-volatile-bitfields,
+    -free,
+    -fipa-pta,
+    -march=*,
+    -mabi=*,
+    -mcpu=*
+  ]
+Diagnostics:
+  Suppress:
+    - pp_including_mainfile_in_preamble
+    - pp_expr_bad_token_start_expr
+    - redefinition_different_typedef
+    - main_returns_nonint
+"""
+
+with open(".glangd", "w") as f:
+    _ = f.write(clangd_config)
+
 
 _ = subprocess.run(["pio","run", "--target", "compiledb"])
